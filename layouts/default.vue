27f3c8b1e4b916bed0f68b56feddd4abe2a9880d
rhynelgit <80267771+rhynelgit@users.noreply.github.com>
2021-10-05 02:04:59 +0800

    Update Commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,13 @@
 <template>
     <v-app>
-      <hero />
       <v-main class="main">
-          <div class="main__content">
-              <wizard-steps />
-              <nuxt></nuxt>
-          </div>
+       
       </v-main>
     </v-app>
 </template>
 
 <script>
-import WizardSteps from '~/components/shared/wizard-steps/WizardSteps';
-import Hero from '~/components/shared/hero/Hero';
-
 export default {
-    components: {
-        WizardSteps,
-        Hero
-    }
+    components: {}
 }
 </script>
--- layouts/empty.vue
+++ layouts/empty.vue
@@ -5,13 +5,9 @@
 </template>
 
 <script>
-import WizardSteps from '~/components/shared/wizard-steps/WizardSteps';
-import Hero from '~/components/shared/hero/Hero';
 
 export default {
     components: {
-        WizardSteps,
-        Hero
     }
 }
 </script>
