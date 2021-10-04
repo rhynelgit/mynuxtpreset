6c53dee8aafd9f176f06e04f2dd30f2c7e19c4a1
rhynelgit <80267771+rhynelgit@users.noreply.github.com>
2021-10-05 02:02:51 +0800

    Scrap

--- components/shared/hero/Hero.vue
+++
@@ -1,7 +0,0 @@
-<template>
-   <aside class="qm-hero">
-        <div class="container">
-            <h4 class="qm-hero__title">QuickMarketing Campaigns</h4>
-        </div>
-   </aside>
-</template>
